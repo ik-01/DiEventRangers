df76af3126da66224d060331c26d6d4c75816f2b
ik-01 <117114145+ik-01@users.noreply.github.com>
2023-01-19 15:56:17 +0500

    Update DvElementQTE.h

--- 010-Template/DvElements/DvElementQTE.h
+++ 010-Template/DvElements/DvElementQTE.h
@@ -1,9 +1,51 @@
+enum <uint32> QTEType
+{
+	PressPrompt = 0,
+	Mash,
+	RedCircle,
+	TheEndVariant,
+	Unknown,
+};
+
+enum <uint32> QTEButton
+{
+	A = 0,
+	B,
+	X,
+	Y,
+	LB_RB,
+	LB,
+	RB,
+	MashA,
+	MashB,
+	MashX,
+	MashY,
+	MashLB,
+	MashRB,
+	Unknown1,
+	Unknown2,
+	Unknown3,
+};
+
 typedef struct
 {
-    dvString elementName;
-    uint32 field_40;
-    float frameStart;
-    float frameEnd;
-    char data[0x194];
+	dvString elementName;
+	uint32 field_40;
+	float frameStart;
+	float frameEnd;
+	uint32 field_4c;
+	uint32 field_50;
+	uint32 field_54;
+	uint32 field_58;
+	uint32 field_5c;
+	QTEType field_60;
+	QTEButton field_64;
+	float field_68[7];	//	Contains QTE params like ring thickness, speed, etc.
+	uint32 field_84;
+	uint32 field_88;
+	char field_8c[0x44];
+	float field_d0;
+	float field_d4;
+	char field_d8[0x108]
     
 }DvElementQTE<optimize=false, read=Str("%s", elementName.str), name="DvElementQTE">;
