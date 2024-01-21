3606803ce7dfa232c7e9ac761d06413539c1a78e
ik-01 <117114145+ik-01@users.noreply.github.com>
2024-01-21 16:38:43 +0500

    Update DvElementBossName.h

--- 010-Template/DvElements/DvElementBossName.h
+++ 010-Template/DvElements/DvElementBossName.h
@@ -1,7 +1,17 @@
+enum <uint32> boss_name_type
+{
+    Giant = 0,
+    Dragon,
+    Knight,
+    Rifle,
+    The_End,
+    RifleBeast
+};
+
 typedef struct
 {
 
 	uint32 field_00;
-    uint32 field_04;
+    boss_name_type boss_name <name= "Boss Name">;
     
 }DvElementBossName<optimize=false, name="DvElementBossName">;
